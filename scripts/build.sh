ed3218e169db57886f5a3f39634d56a90738f6af
Yamagishi Kazutoshi <ykzts@desire.sh>
2019-11-22 00:22:45 +0900

    feat: improve dev profile (#42)

--- scripts/build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-set -ex
-
-cd $(mktemp -d)
-wget https://github.com/tootsuite/mastodon/archive/master.tar.gz
-tar xf master.tar.gz
-cd mastodon-master
-docker build --add-host="archive.ubuntu.com:2001:df0:2ed:feed::feed" --tag asia.gcr.io/ykzts-technology/mastodon:$(date -u +'%Y%m%d%H%M%S') .
