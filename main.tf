357da0e7357f1d9f18048c795864bc6abe9305eb
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-04-10 11:01:53 +0000

    ci(terraform): introduce terraform

---
+++ main.tf
@@ -0,0 +1,78 @@
+terraform {
+  cloud {
+    organization = "ykzts-technology"
+
+    workspaces {
+      name = "mastodon"
+    }
+  }
+}
+
+locals {
+  cluster_type           = "mastodon"
+  master_auth_subnetwork = "mastodon-master-subnet"
+  network_name           = "mastodon-network"
+  pods_range_name        = "ip-range-pods-mastodon"
+  subnet_name            = "mastodon-subnet"
+  subnet_names           = [for subnet_self_link in module.vpc.subnets_self_links : split("/", subnet_self_link)[length(split("/", subnet_self_link)) - 1]]
+  svc_range_name         = "ip-range-svc-mastodon"
+}
+
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+provider "google-beta" {
+  project = var.project_id
+  region  = var.region
+}
+
+data "google_client_config" "default" {}
+
+module "vpc" {
+  source  = "terraform-google-modules/network/google"
+  version = "~> 5.0"
+
+  network_name = ""
+  project_id   = var.project_id
+  secondary_ranges = {
+    (local.subnet_name) = [
+      {
+        range_name    = local.pods_range_name
+        ip_cidr_range = "192.168.0.0/18"
+      },
+      {
+        range_name    = local.svc_range_name
+        ip_cidr_range = "192.168.64.0/18"
+      },
+    ]
+  }
+  subnets = [
+    {
+      subnet_name   = local.subnet_name
+      subnet_ip     = "10.0.0.0/17"
+      subnet_region = var.region
+    },
+    {
+      subnet_name   = local.master_auth_subnetwork
+      subnet_ip     = "10.60.0.0/17"
+      subnet_region = var.region
+    },
+  ]
+}
+
+module "gke" {
+  source  = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-public-cluster"
+  version = "~> 20.0"
+
+  enable_vertical_pod_autoscaling = true
+  ip_range_pods                   = local.pods_range_name
+  ip_range_services               = local.svc_range_name
+  name                            = "${local.cluster_type}-cluster"
+  network                         = module.vpc.network_name
+  project_id                      = var.project_id
+  region                          = var.region
+  release_channel                 = "REGULAR"
+  subnetwork                      = local.subnet_names[index(module.vpc.subnets_names, local.subnet_name)]
+}
