4378c1b1491e4bd5396c1d49be37415f9205fe14
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-04-14 02:29:24 +0900

    fix(terraform): remove unused variables (#226)

--- variables.tf
+++ variables.tf
@@ -39,14 +39,6 @@ variable "smtp_password" {
   type = string
 }
 
-variable "storage_access_key" {
-  type = string
-}
-
-variable "storage_secret_key" {
-  type = string
-}
-
 variable "storage_bucket_name" {
   type = string
 }
