61bd8d377e8c51202fd16ab0524abeb1d6a72c2b
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-04-14 23:06:08 +0900

    feat(sa): add service account for terraform (#231)

--- sa.tf
+++ sa.tf
@@ -1,3 +1,27 @@
+module "service_accounts" {
+  source  = "terraform-google-modules/service-accounts/google"
+  version = "4.1.1"
+
+  generate_keys = true
+  names         = ["terraform"]
+  project_id    = var.project_id
+  project_roles = [
+    "${var.project_id}=>roles/cloudsql.admin",
+    "${var.project_id}=>roles/compute.networkAdmin",
+    "${var.project_id}=>roles/compute.securityAdmin",
+    "${var.project_id}=>roles/compute.viewer",
+    "${var.project_id}=>roles/container.clusterAdmin",
+    "${var.project_id}=>roles/container.developer",
+    "${var.project_id}=>roles/dns.admin",
+    "${var.project_id}=>roles/iam.serviceAccountAdmin",
+    "${var.project_id}=>roles/iam.serviceAccountKeyAdmin",
+    "${var.project_id}=>roles/iam.serviceAccountUser",
+    "${var.project_id}=>roles/resourcemanager.projectIamAdmin",
+    "${var.project_id}=>roles/storage.hmacKeyAdmin",
+    "${var.project_id}=>roles/storage.admin",
+  ]
+}
+
 resource "random_id" "gcs_service_account_suffix" {
   byte_length = 4
 }
