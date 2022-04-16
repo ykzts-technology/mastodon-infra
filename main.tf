750f9c0cb0e315f4b9801a08db9e757b2ea2db4f
Yamagishi Kazutoshi <ykzts@desire.sh>
2022-04-16 13:45:55 +0000

    build(deps): update postgresql to v14

--- main.tf
+++ main.tf
@@ -129,7 +129,7 @@ module "sql-db" {
       value = "6800"
     },
   ]
-  database_version    = "POSTGRES_9_6"
+  database_version    = "POSTGRES_14"
   db_charset          = "UTF8"
   db_collation        = "en_US.UTF8"
   db_name             = "postgres"
