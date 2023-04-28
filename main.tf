87ec31618a2e7f27e9e8ff4ba4ec385d0bf2f7b3
Yamagishi Kazutoshi <ykzts@desire.sh>
2023-04-28 21:39:58 +0900

    fix(gke): use default service account

--- main.tf
+++ main.tf
@@ -29,6 +29,7 @@ module "gke" {
   source  = "terraform-google-modules/kubernetes-engine/google//modules/beta-autopilot-public-cluster"
   version = "25.0.0"
 
+  create_service_account          = false
   enable_vertical_pod_autoscaling = true
   ip_range_pods                   = local.pods_range_name
   ip_range_services               = local.svc_range_name
